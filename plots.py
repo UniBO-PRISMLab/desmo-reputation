17f2d731de2ca3cff0b8fec32b5aa463cb01ee7a
Stradivarius <fede.selmer@gmail.com>
2024-05-05 21:38:53 +0200

    Final plots for real

--- plots.py
+++ plots.py
@@ -3,6 +3,7 @@ import pandas as pd
 import os, sys
 import glob
 import matplotlib.pyplot as plt
+import matplotlib.lines as lns
 import seaborn as sns
 from tqdm import tqdm
 import numpy as np
@@ -11,7 +12,7 @@ import matplotlib.ticker as mtick
 
 # %%
 # Constants
-parent_folder  = "results-test"
+parent_folder  = "results-test-small"
 all_files = glob.glob(os.path.join(parent_folder, "*.csv"))
 HEADERS = ["Epoch", "sources", "malign", "ban_sources", "ban_malign", "consensus", "rep_benign", "rep_malign"]
 print("Found {} results".format(len(all_files)))
@@ -22,7 +23,7 @@ _REPLACE_ = True
 # %%
 rc_params = {
     'font.size': 26,
-    'legend.fontsize': 12,
+    'legend.fontsize': 14,
     'legend.title_fontsize': 14,
     'figure.figsize': (7, 5),
     'axes.facecolor': '#FFFFFF',
@@ -70,9 +71,6 @@ df = pd.concat(_li, axis=0, ignore_index=True)
 print("We ended up with {} rows".format(len(df)))
 
 # %%
-
-
-# %%
 # Calculate Precision and Recall
 print("Calculate precision and recall")
 df['Recall'] = df.apply(lambda row: (row['ban_malign'] / row['malign'] if row['malign'] else 0), axis=1)
@@ -89,7 +87,9 @@ print("Consensus Accuracy done")
 
 # %%
 # Reduce dataset for tests XXX
+df_tail = df.drop(df[(df['Epoch'] < 2000)].index) # FIXME should be 2000
 df_goodo = df.drop(df[(df['Ratio Malicious Oracles'] != 0.0)].index)
+df_goodo2 = df_tail.drop(df_tail[(df_tail['Ratio Malicious Oracles'] != 0.0)].index)
 df_def = df_goodo.drop(df_goodo[(df_goodo.Algorithm != 2)].index) # This is only considering our algorithm
 #df_endtime = df_def.drop(df_def[(df_def.Epoch < 39)].index) # This is only conisdering the very last stage of the simulation,rather than evry single epoch
 
@@ -122,8 +122,19 @@ img_name = "min_rep_rec_over_time.png"
 if _REPLACE_ or not os.path.exists(img_name):
     print ("Doing " + img_name)
     fig, ax  = plt.subplots()
-    sns.lineplot(data=df_def, x='Epoch', y='Recall', hue='rep_min', palette=zonia_palette)
-    plt.legend(title='Min Reputation', loc='lower right')
+    sns.lineplot(data=df_def, x='Epoch', y='Recall', hue='rep_min', palette=zonia_palette, linewidth = 3)
+    plt.legend(title='$\omega$', loc='upper left') # omega is the Min reputation for kickout
+    plt.tight_layout()
+    plt.savefig(img_name, dpi=300)
+    # plt.show()
+    print (img_name + "Done")
+img_name = "min_rep_rec_over_time_noerror.png"
+if _REPLACE_ or not os.path.exists(img_name):
+    print ("Doing " + img_name)
+    fig, ax  = plt.subplots()
+    sns.lineplot(data=df_def, x='Epoch', y='Recall', hue='rep_min', palette=zonia_palette, linewidth = 3, errorbar=None)
+    plt.legend(title='$\omega$', loc='upper left') # omega is the Min reputation for kickout
+    plt.tight_layout()
     plt.savefig(img_name, dpi=300)
     # plt.show()
     print (img_name + "Done")
@@ -135,8 +146,18 @@ if _REPLACE_ or not os.path.exists(img_name):
 img_name = "min_rep_prec_over_time.png"
 if _REPLACE_ or not os.path.exists(img_name):
     fig, ax  = plt.subplots()
-    sns.lineplot(data=df_def, x='Epoch', y='Precision', hue='rep_min', palette=zonia_palette)
-    plt.legend(title='Min Reputation', loc='lower right')
+    sns.lineplot(data=df_def, x='Epoch', y='Precision', hue='rep_min', palette=zonia_palette, linewidth = 3)
+    plt.legend(title='$\omega$', loc='upper left') # omega is the Min reputation for kickout
+    plt.tight_layout()
+    plt.savefig(img_name, dpi=300)
+    # plt.show()
+    print (img_name + "Done")
+img_name = "min_rep_prec_over_time_noerror.png"
+if _REPLACE_ or not os.path.exists(img_name):
+    fig, ax  = plt.subplots()
+    sns.lineplot(data=df_def, x='Epoch', y='Precision', hue='rep_min', palette=zonia_palette, linewidth = 3, errorbar=None)
+    plt.legend(title='$\omega$', loc='upper left') # omega is the Min reputation for kickout
+    plt.tight_layout()
     plt.savefig(img_name, dpi=300)
     # plt.show()
     print (img_name + "Done")
@@ -152,8 +173,19 @@ img_name = "min_rep_rec_over_nummal.png"
 if _REPLACE_ or not os.path.exists(img_name):
     fig, ax  = plt.subplots()
     print ("Doing " + img_name)
-    sns.lineplot(data=df_def_4, x='Epoch', y='Recall', hue='ratio_malign', palette=zonia_palette)
-    plt.legend(title='Ratio Malicious', loc='upper left')
+    sns.lineplot(data=df_def_4, x='Epoch', y='Recall', hue='ratio_malign', palette=zonia_palette, linewidth = 3)
+    plt.legend(title='RMI', loc='upper left')
+    plt.tight_layout()
+    plt.savefig(img_name, dpi=300)
+    # plt.show()
+    print (img_name + "Done")
+img_name = "min_rep_rec_over_nummal_noerror.png"
+if _REPLACE_ or not os.path.exists(img_name):
+    fig, ax  = plt.subplots()
+    print ("Doing " + img_name)
+    sns.lineplot(data=df_def_4, x='Epoch', y='Recall', hue='ratio_malign', palette=zonia_palette, linewidth = 3, errorbar=None)
+    plt.legend(title='RMI', loc='upper left')
+    plt.tight_layout()
     plt.savefig(img_name, dpi=300)
     # plt.show()
     print (img_name + "Done")
@@ -165,8 +197,19 @@ img_name = "min_rep_prec_over_nummal.png"
 if _REPLACE_ or not os.path.exists(img_name):
     fig, ax  = plt.subplots()
     print ("Doing " + img_name)
-    sns.lineplot(data=df_def_4, x='Epoch', y='Precision', hue='ratio_malign', palette=zonia_palette)
-    plt.legend(title='Ratio Malicious', loc='upper left')
+    sns.lineplot(data=df_def_4, x='Epoch', y='Precision', hue='ratio_malign', palette=zonia_palette, linewidth = 3)
+    plt.legend(title='RMI', loc='upper left')
+    plt.tight_layout()
+    plt.savefig(img_name, dpi=300)
+    # plt.show()
+    print (img_name + "Done")
+img_name = "min_rep_prec_over_nummal_noerror.png"
+if _REPLACE_ or not os.path.exists(img_name):
+    fig, ax  = plt.subplots()
+    print ("Doing " + img_name)
+    sns.lineplot(data=df_def_4, x='Epoch', y='Precision', hue='ratio_malign', palette=zonia_palette, linewidth = 3, errorbar=None)
+    plt.legend(title='RMI', loc='upper left')
+    plt.tight_layout()
     plt.savefig(img_name, dpi=300)
     # plt.show()
     print (img_name + "Done")
@@ -179,13 +222,21 @@ img_name = "rep_ben_mal.png"
 if _REPLACE_ or not os.path.exists(img_name):
     fig, ax  = plt.subplots()
     print ("Doing " + img_name)
-    ab = sns.lineplot(data=df_def, x='Epoch', y='rep_benign', hue='ratio_malign', palette=zonia_palette)
+    ab = sns.lineplot(data=df_def, x='Epoch', y='rep_malign', hue='ratio_malign', palette=zonia_palette, linewidth = 3,  legend=None, errorbar=None)
     for l in ab.lines:
         l.set_linestyle("--")
-    aa = sns.lineplot(data=df_def, x='Epoch', y='rep_malign', hue='ratio_malign', palette=zonia_palette)
+    
+    aa = sns.lineplot(data=df_def, x='Epoch', y='rep_benign', hue='ratio_malign', palette=zonia_palette, linewidth = 3, errorbar=None)
+    legend1 = plt.legend(title='RMI', loc='center right')
 
-    plt.legend(title='Ratio of malicious indexers', loc='upper left')
+    custom_lines = [lns.Line2D([0], [0], color='black', lw=3),
+                lns.Line2D([0], [0], color='black', linestyle='--', lw=3)]
+    legend2 = ax.legend(custom_lines, ['Malicious', 'Honest'],title='Indexers', loc='lower left')
+    plt.gca().add_artist(legend1)
+
+    
     plt.ylabel("Average reputation", fontsize=16)
+    plt.tight_layout()
     plt.savefig(img_name, dpi=300)
     # plt.show()
     print (img_name + "Done")
@@ -277,6 +328,12 @@ if (_REPLACE_ or not os.path.exists(img_name)) and not _DISCARD_:
 
 
 # %%
+import matplotlib.ticker as tkr
+def numfmt(x, pos): # your custom formatter function: divide by 100.0
+    s = '{}'.format(float(x) / 100.0)
+    return s
+
+# %%
 print("Doing the bars...")
 df_bar = df_goodo.groupby(['rep_min', 'ratio_malign', 'Algorithm', 'Arrival'])['Consensus Accuracy'].agg(['sum','count']).reset_index()
 df_bar['Accuracy'] = df_bar.apply(lambda x: float(x['sum']) / float(x['count']) , axis=1)
@@ -304,7 +361,7 @@ if _REPLACE_ or not os.path.exists(img_name):
 
     plt.xticks(xvals)
     xvals = xvals - (len(algos) * len(arrs) * width / 2) - (width/2)
-    plt.xlabel('Percentage of Malicious Sources', fontsize=16)
+    plt.xlabel('RMI', fontsize=16)
     plt.ylabel('Accuracy', fontsize=16)
     plt.rcParams['mathtext.default'] = 'regular'
     for i, algo in enumerate(algos):
@@ -320,22 +377,24 @@ if _REPLACE_ or not os.path.exists(img_name):
             #     ax.errorbar(pos, y, err, lw = 2,
             #                 capsize = 4, capthick = 2,
             #                 color = "red")
-    plt.legend(loc='upper center', bbox_to_anchor=(0.48, 1.25), ncol=3, labels=['Med (uniform)', 'Med (bursty)', 'Med-R (uniform)', 'Med-R (bursty)', 'Med-RB (uniform)', 'Med-RB (bursty)'])
+    plt.legend(loc='upper center', bbox_to_anchor=(0.48, 1.20), ncol=3, labels=['Med (uniform)', 'Med (bursty)', 'Med-R (uniform)', 'Med-R (bursty)', 'Med-RB (uniform)', 'Med-RB (bursty)'])
     plt.ylim([0,1.1])
-    plt.gca().xaxis.set_major_formatter(mtick.PercentFormatter(decimals=1))
-    plt.tight_layout()
+    # plt.gca().xaxis.set_major_formatter(mtick.PercentFormatter(decimals=1))
+    plt.gca().xaxis.set_major_formatter(tkr.FuncFormatter(numfmt)) 
+
     # plt.show()
     plt.savefig(img_name, dpi=200)
     print(img_name + " Done!")
 
+# %%
 print("Doing the bars...")
 df_bar = df_goodo2.groupby(['rep_min', 'ratio_malign', 'Algorithm', 'Arrival'])['Consensus Accuracy'].agg(['sum','count']).reset_index()
 df_bar['Accuracy'] = df_bar.apply(lambda x: float(x['sum']) / float(x['count']) , axis=1)
 df_bar['ratio_malign'] = df_bar.apply(lambda x: float(x['ratio_malign'] * 100.0), axis=1)
 
-
 df_bar = df_bar.groupby(['ratio_malign', 'Algorithm', 'Arrival'])['Accuracy'].agg(['mean','std']).reset_index()
 
+# %%
 img_name = "desmo_accuracy_tail.png"
 if _REPLACE_ or not os.path.exists(img_name):
     print ("Doing " + img_name)
@@ -353,7 +412,7 @@ if _REPLACE_ or not os.path.exists(img_name):
 
     plt.xticks(xvals)
     xvals = xvals - (len(algos) * len(arrs) * width / 2) - (width/2)
-    plt.xlabel('Percentage of Malicious Sources', fontsize=16)
+    plt.xlabel('RMI', fontsize=16)
     plt.ylabel('Accuracy', fontsize=16)
     plt.rcParams['mathtext.default'] = 'regular'
     for i, algo in enumerate(algos):
@@ -369,10 +428,9 @@ if _REPLACE_ or not os.path.exists(img_name):
             #     ax.errorbar(pos, y, err, lw = 2,
             #                 capsize = 4, capthick = 2,
             #                 color = "red")
-    plt.legend(loc='upper center', bbox_to_anchor=(0.48, 1.25), ncol=3, labels=['Med (uniform)', 'Med (bursty)', 'Med-R (uniform)', 'Med-R (bursty)', 'Med-RB (uniform)', 'Med-RB (bursty)'])
+    plt.legend(loc='upper center', bbox_to_anchor=(0.48, 1.20), ncol=3, labels=['Med (uniform)', 'Med (bursty)', 'Med-R (uniform)', 'Med-R (bursty)', 'Med-RB (uniform)', 'Med-RB (bursty)'])
     plt.ylim([0,1.1])
-    plt.gca().xaxis.set_major_formatter(mtick.PercentFormatter(decimals=1))
-    plt.tight_layout()
+    plt.gca().xaxis.set_major_formatter(tkr.FuncFormatter(numfmt))
     # plt.show()
     plt.savefig(img_name, dpi=200)
     print(img_name + " Done!")
@@ -402,18 +460,20 @@ if _REPLACE_ or not os.path.exists(img_name):
     dx = dy = 0.75
     dz = values.flatten()
 
-    ax.bar3d(xpos, ypos, zpos, dx, dy, dz, shade=True)
+    ax.bar3d(xpos, ypos, zpos, dx, dy, dz, shade=True, color="#1B2D41")
     ax.set_zlim(0, 1)
 
-    ax.set_xticks(np.arange(len(xvals)))
-    ax.set_yticks(np.arange(len(yvals)))
+    xticks = np.arange(len(xvals))
+    plt.xticks(xticks + 0.33, xticks)
+    yticks = np.arange(len(yvals))
+    plt.yticks(yticks + 0.5, yticks)
     ax.set_xticklabels(xvals)
     ax.set_yticklabels(yvals)
 
-    ax.set_xlabel('Ratio of Malicious Oracles')
-    ax.set_ylabel('Ratio of Malicious Indexers')
+    ax.set_xlabel('RMO', labelpad=10)
+    ax.set_ylabel('RMI', labelpad=10)
     ax.set_zlabel('Accuracy')
-    plt.title('Algorithm = Med')
+    #plt.title('Algorithm = Med')
     plt.savefig(img_name, dpi=200)
 
 # %%
@@ -421,6 +481,8 @@ img_name = "3d_accuracy_2.png"
 if _REPLACE_ or not os.path.exists(img_name):
     this_df = df.drop(df[(df.Algorithm != 1)].index)
 
+    print(this_df)
+
     xvals = sorted(this_df['Ratio Malicious Oracles'].unique())
     yvals = sorted(this_df['ratio_malign'].unique(), reverse=True)
     values = np.zeros(shape=(len(xvals), len(yvals)))
@@ -442,17 +504,20 @@ if _REPLACE_ or not os.path.exists(img_name):
     dx = dy = 0.75
     dz = values.flatten()
 
-    ax.bar3d(xpos, ypos, zpos, dx, dy, dz, shade=True)
+    ax.bar3d(xpos, ypos, zpos, dx, dy, dz, shade=True, color='#466C82')
 
-    ax.set_xticks(np.arange(len(xvals)))
-    ax.set_yticks(np.arange(len(yvals)))
+    #ax.set_xticks(np.arange(len(xvals)))
+    xticks = np.arange(len(xvals))
+    plt.xticks(xticks + 0.33, xticks)
+    yticks = np.arange(len(yvals))
+    plt.yticks(yticks + 0.5, yticks)
     ax.set_xticklabels(xvals)
     ax.set_yticklabels(yvals)
     ax.set_zlim(0, 1)
-    ax.set_xlabel('Ratio of Malicious Oracles')
-    ax.set_ylabel('Ratio of Malicious Indexers')
+    ax.set_xlabel('RMO', labelpad=10)
+    ax.set_ylabel('RMI', labelpad=10)
     ax.set_zlabel('Accuracy')
-    plt.title('Algorithm = Med-R')
+    #plt.title('Algorithm = Med-R')
     plt.savefig(img_name, dpi=200)
 
 # %%
@@ -460,6 +525,8 @@ img_name = "3d_accuracy_3.png"
 if _REPLACE_ or not os.path.exists(img_name):
     this_df = df.drop(df[(df.Algorithm != 2)].index)
 
+    print(this_df)
+
     xvals = sorted(this_df['Ratio Malicious Oracles'].unique())
     yvals = sorted(this_df['ratio_malign'].unique(), reverse=True)
     values = np.zeros(shape=(len(xvals), len(yvals)))
@@ -481,18 +548,19 @@ if _REPLACE_ or not os.path.exists(img_name):
     dx = dy = 0.75
     dz = values.flatten()
 
-    ax.bar3d(xpos, ypos, zpos, dx, dy, dz, shade=True)
+    ax.bar3d(xpos, ypos, zpos, dx, dy, dz, shade=True, color="#87A0B3")
     ax.set_zlim(0, 1)
 
-    ax.set_xticks(np.arange(len(xvals)))
-    ax.set_yticks(np.arange(len(yvals)))
+    xticks = np.arange(len(xvals))
+    plt.xticks(xticks + 0.33, xticks)
+    yticks = np.arange(len(yvals))
+    plt.yticks(yticks + 0.5, yticks)
     ax.set_xticklabels(xvals)
     ax.set_yticklabels(yvals)
 
-    ax.set_xlabel('Ratio of Malicious Oracles')
-    ax.set_ylabel('Ratio of Malicious Indexers')
+    ax.set_xlabel('RMO', labelpad=10)
+    ax.set_ylabel('RMI', labelpad=10)
     ax.set_zlabel('Accuracy')
-    plt.title('Algorithm = Med-RB')
     plt.savefig(img_name, dpi=200)
 
 # %%
